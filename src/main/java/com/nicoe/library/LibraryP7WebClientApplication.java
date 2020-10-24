8629d1fd0ba3198b24dcae0d50fd90713dd740c9
Nico Evgl <nico.evangelista33@gmail.com>
2020-10-24 13:02:04 +0200

    first commit - build Maven Project / pom.xml

---
+++ src/main/java/com/nicoe/library/LibraryP7WebClientApplication.java
@@ -0,0 +1,13 @@
+package com.nicoe.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryP7WebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryP7WebClientApplication.class, args);
+	}
+
+}
