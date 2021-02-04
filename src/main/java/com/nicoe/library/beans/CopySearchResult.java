4bd686b816bfc9efacf58978b7531b662aa50f59
Nico Evgl <nico.evangelista33@gmail.com>
2021-02-04 20:21:00 +0100

    new attributes in CopySearchResult

--- src/main/java/com/nicoe/library/beans/CopySearchResult.java
+++ src/main/java/com/nicoe/library/beans/CopySearchResult.java
@@ -1,10 +1,16 @@
 package com.nicoe.library.beans;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class CopySearchResult implements Serializable {
     public String title;
     public Integer nbAvailable;
+    Date expectedReturnDate;
+    Integer nbReservationsMade;
+    Integer nbPossibleReservations;
+    Integer bookId;
+
 
     public String getTitle() {
         return title;
@@ -20,5 +26,37 @@ public class CopySearchResult implements Serializable {
 
     public void setNbAvailable(Integer nbAvailable) {
         this.nbAvailable = nbAvailable;
+    }
+
+    public Date getExpectedReturnDate() {
+        return expectedReturnDate;
+    }
+
+    public void setExpectedReturnDate(Date expectedReturnDate) {
+        this.expectedReturnDate = expectedReturnDate;
+    }
+
+    public Integer getNbReservationsMade() {
+        return nbReservationsMade;
+    }
+
+    public void setNbReservationsMade(Integer nbReservationsMade) {
+        this.nbReservationsMade = nbReservationsMade;
+    }
+
+    public Integer getNbPossibleReservations() {
+        return nbPossibleReservations;
+    }
+
+    public void setNbPossibleReservations(Integer nbPossibleReservations) {
+        this.nbPossibleReservations = nbPossibleReservations;
+    }
+
+    public Integer getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Integer bookId) {
+        this.bookId = bookId;
     }
 }
