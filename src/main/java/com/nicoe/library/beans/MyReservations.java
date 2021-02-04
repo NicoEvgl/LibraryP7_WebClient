7b3a94d9a8a007445c4e904df7957d1619e0fee2
Nico Evgl <nico.evangelista33@gmail.com>
2021-02-04 20:19:59 +0100

    add beans Reservation & MyReservations

---
+++ src/main/java/com/nicoe/library/beans/MyReservations.java
@@ -0,0 +1,53 @@
+package com.nicoe.library.beans;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class MyReservations implements Serializable {
+
+    Integer reservationId;
+    Integer bookId;
+    String title;
+    Integer rank;
+    Date returnDate;
+
+    public Integer getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(Integer reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public Integer getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Integer bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getRank() {
+        return rank;
+    }
+
+    public void setRank(Integer rank) {
+        this.rank = rank;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+}
---
+++ src/main/java/com/nicoe/library/beans/Reservation.java
@@ -0,0 +1,22 @@
+package com.nicoe.library.beans;
+
+public class Reservation {
+    Integer bookId;
+    Integer userId;
+
+    public Integer getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Integer bookId) {
+        this.bookId = bookId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+}
