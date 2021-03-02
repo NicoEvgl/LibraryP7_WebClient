beb02f70be5c08076c5beb45646e7025a8dadfd0
Nico Evgl <nico.evangelista33@gmail.com>
2021-03-02 18:21:38 +0100

    fix Reservation (implements Serializable)

--- src/main/java/com/nicoe/library/beans/Reservation.java
+++ src/main/java/com/nicoe/library/beans/Reservation.java
@@ -1,6 +1,8 @@
 package com.nicoe.library.beans;
 
-public class Reservation {
+import java.io.Serializable;
+
+public class Reservation implements Serializable {
     Integer bookId;
     Integer userId;
 
